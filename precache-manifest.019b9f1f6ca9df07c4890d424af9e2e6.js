7862ade3650184695d8cdbf89864e15e1761aa4a
Timur Zakirov <1234tima.g@gmail.com>
2020-05-02 15:47:06 +0300

    Updates

---
+++ precache-manifest.019b9f1f6ca9df07c4890d424af9e2e6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cfcd4fbcce48f704a27586d9d0e55932",
+    "url": "/react-domashka-second/index.html"
+  },
+  {
+    "revision": "06ef21a5cc5caf59e241",
+    "url": "/react-domashka-second/static/js/2.456eb57b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-domashka-second/static/js/2.456eb57b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d4c133c4dd8d60995166",
+    "url": "/react-domashka-second/static/js/main.52f70d36.chunk.js"
+  },
+  {
+    "revision": "24de03fa7f6fe369ebb6",
+    "url": "/react-domashka-second/static/js/runtime-main.0b960c9f.js"
+  }
+]);
